4bb3fecb793ee9005c60c5d6c9cd45315c2d6516
Chris Slade <crslade@gmail.com>
2018-09-24 23:52:19 +0000

    Add address

--- animal.cpp
+++ animal.cpp
@@ -3,19 +3,60 @@
 
 using namespace std;
 
+class Address {
+  public:
+    Address(string address);
+    void printInfo();
+    ~Address();
+  private:
+    string address;
+};
+
 class Animal {
 public:
   Animal(string n, string o);
   void printInfo();
+  void printAddress();
+  void setAddress(string address);
+  ~Animal();
 private:
   string name;
   string owner;
-  
+  Address *address;
 };
+
+
+
+Address::Address(string address) {
+    Address::address = address;
+}
+
+void Address::printInfo() {
+    cout << "Address: " << address << endl;
+}
+
+Address::~Address() {
+    cout << "Bye Address" << endl;
+}
 
 Animal::Animal(string n, string o) {
     name = n;
     owner = o;
+    address = NULL;
+}
+
+void Animal::setAddress(string addr) {
+    Address *a = new Address(addr);
+    address = a;
+}
+
+void Animal::printAddress() {
+    address->printInfo();
+}
+
+Animal::~Animal() {
+    delete address;
+    cout << "Bye Bye: " << name << endl;
 }
 
 void Animal::printInfo() {
@@ -24,14 +65,20 @@ void Animal::printInfo() {
 }
 
 
+Animal* createMary() {
+    Animal *mary = new Animal("Mary", "Losse");
+    mary->setAddress("Tonga");
+    return mary;
+}
+
 int main(int argc, char **argv) {
-    
-    //string fred;
-    //cout << "Enter something for fred: ";
-    //cin >> fred;
-    
     Animal fred("Fred", "Bro. Slade");
     fred.printInfo();
-    
+    Animal *mary2 = createMary();
+    //(*mary2).printInfo();
+    mary2->printInfo();
+    mary2->printAddress();
+    delete mary2;
     cout << "Hello Class: " << endl;
 }
+
